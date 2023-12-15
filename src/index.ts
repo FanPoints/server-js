7ddc28ba4aea6e8669200a8dcd2addd998dd5661
Tobia Ochsner <tobia.ochsner@hotmail.com>
2023-12-16 00:43:08 +0100

    Update docs.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export type { ClientConfig } from './FanPointsClient';
 export { default as FanPointsClient, createClient } from './FanPointsClient';
+
+export { UserModule } from './UserModule';
+export { FanPointsModule } from './FanPointsModule';
+export { StatusPointsModule } from './StatusPointsModule';
