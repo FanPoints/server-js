623bf57c286254415437338377eb4ca2c8048036
Tobia Ochsner <tobia.ochsner@hotmail.com>
2023-12-18 13:42:12 +0100

    Remove secrets from version control.

--- doppler/backendConfig.ts
+++ doppler/backendConfig.ts
@@ -1,9 +1,7 @@
 const config = {
-    oAuthDomain:
-        'https://main-user-exttest-pool-domain.auth.eu-central-1.amazoncognito.com/oauth2/token',
-    apiEndpoint:
-        'https://6sj4xf53arh3zch5luwpb6fv4m.appsync-api.eu-central-1.amazonaws.com/graphql',
-    region: 'eu-central-1',
+    oAuthDomain: 'https://{{.AWS_COGNITO_OAUTH_DOMAIN}}/oauth2/token',
+    apiEndpoint: '{{.AWS_APPSYNC_GRAPHQL_API_URL}}',
+    region: '{{.AWS_APPSYNC_REGION}}',
 } as const;
 
 export type Config = typeof config;
