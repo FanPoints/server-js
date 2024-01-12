790b0b26a3f4b627f230e6e239a2bf7ffaf0abe3
Tobia Ochsner <tobia.ochsner@hotmail.com>
2024-01-12 20:33:12 +0100

    Remove secrets from index.js.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
-import { createClient } from './FanPointsClient';
-
 export type { ClientConfig } from './FanPointsClient';
 export { default as FanPointsClient, createClient } from './FanPointsClient';
 
 export { UserModule } from './UserModule';
 export { FanPointsModule } from './FanPointsModule';
 export { StatusPointsModule } from './StatusPointsModule';
-
-const client = createClient({
-    projectId: 'FCB',
-    clientId: '2nmql0lj74lof47i0dd3uk4etb',
-    secret: 'lq8rag6of5pnqrllehgjhfs1ul9jvnuuldp6g1btnhskmuommb1',
-});
-
-const func = async () => {
-    const a = await client.fanPoints.getBalance('U1');
-    console.log(a);
-};
-
-func();
